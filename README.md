3e728d268ae84247b969ea5981fba3d3835494ca
pedro2731 <79153764+pedro2731@users.noreply.github.com>
2023-06-02 08:59:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Typescript-Class
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ptqcyn)
---
+++ index.ts
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+//const appDiv: HTMLElement = document.getElementById('app');
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+//inferencia de datos
+const a: number = 10;
+let b = 10;
+
+// Booleans
+let isSuperman = true;
+let isBatman = false;
+
+isSuperman = 1 + 1 == 2 ? true : false;
+
+console.log(isSuperman);
